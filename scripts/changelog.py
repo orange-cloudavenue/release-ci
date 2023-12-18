f11e8954955bb36abbc5812aeb327c7a40787756
Lisa Lewitanski <lisa.lewitanski@orange.com>
2023-12-18 12:14:42 +0100

    feat: Improve changelog python script

--- scripts/changelog.py
+++ scripts/changelog.py
@@ -2,7 +2,21 @@ import argparse
 import os
 import subprocess
 
+CHANGELOG_GITHUB_TEMPLATE_PATH = ".github/workflows/generate_changelog.yml"
+RELEASE_GITHUB_TEMPLATE_PATH = ".github/workflows/release.yml"
+CHANGELOG_GITHUB_TEMPLATE_NAME = "template_generate_changelog.yml"
+RELEASE_GITHUB_TEMPLATE_NAME = "template_release.yml"
+
+
 def write_template(file_to_write, template_file):
+    """
+    Write a template file with the content of a given template file.
+
+    file_to_write {str}: name of the file to create with data.
+    template_file {str}: name of the template file to get the data.
+
+    return: None
+    """
     try:
         new_file = open(file_to_write, "a")
         new_file.close()
@@ -13,11 +27,32 @@ def write_template(file_to_write, template_file):
         print("Exeption during configuration file creation")
         raise error
 
+
+def create_template_file(template_file_path, template_path, template_file_name):
+    """
+    Create a template file.
+
+    template_file_path {str}:           path of the template
+    template_path {str}:                path where we want to create the template
+    template_file_name {str}:           name of the template file that we want to create
+    """
+    if not os.path.exists(template_file_path):
+        try:
+            template_generate_changelog_path = template_path + template_file_name
+            template_generate_changelog = open(template_generate_changelog_path, "r")
+            write_template(template_file_path, template_generate_changelog)
+            template_generate_changelog.close()
+        except Exception as error:
+            raise error
+
+
 def main():
     """Main function"""
     # Initialize the parser
     parser = argparse.ArgumentParser(description="Setup Changelog")
-    parser.add_argument("-p", "--path", type=str, help="Project path", default=None, required=True)
+    parser.add_argument(
+        "-p", "--path", type=str, help="Project path", default=None, required=True
+    )
 
     # Initialize arguments
     arguments = parser.parse_args()
@@ -25,7 +60,13 @@ def main():
     if not parent_dir:
         return
 
-    # Step 1 - Changelog folders
+    if not parent_dir.endswith("/"):
+        parent_dir += "/"
+
+    curent_dir = os.getcwd()
+    template_path = "/".join(curent_dir.split("/")[:-1]) + "/templates/"
+
+    # Step 1 - Changelog folder
     changelog_folder = os.path.join(parent_dir, ".changelog")
     if not os.path.exists(changelog_folder):
         os.mkdir(changelog_folder)
@@ -48,50 +89,46 @@ def main():
         os.mkdir(workflow_folder)
 
     # Step 2 - .ci submodule
-    p = subprocess.Popen(['git', 'submodule', 'add', '--name', '.ci', 'https://github.com/FrangipaneTeam/release-ci', '.ci'], cwd=parent_dir)
-    p.wait()
+    ci_process = subprocess.Popen(['git', 'submodule', 'add', '--name', '.ci', 'https://github.com/FrangipaneTeam/release-ci', '.ci'], cwd=parent_dir)
+    ci_process.wait()
 
     # Step 3 - changelog configuration
-    generate_changelog = parent_dir + ".github/workflows/generate_changelog.yml"
-    if not os.path.exists(generate_changelog):
-        try:
-            template_generate_changelog = open('template_generate_changelog.yml', 'r')
-            write_template(generate_changelog, template_generate_changelog)
-            template_generate_changelog.close()
-        except Exception as error:
-            raise error
+    template_file_path = parent_dir + CHANGELOG_GITHUB_TEMPLATE_PATH
+    template_file_name = CHANGELOG_GITHUB_TEMPLATE_NAME
+    create_template_file(template_file_path, template_path, template_file_name)
 
     # Step 4 - Release configuration
-    release = parent_dir + ".github/workflows/release.yml"
-    if not os.path.exists(release):
-        try:
-            template_release = open('template_release.yml', 'r')
-            write_template(release, template_release)
-            template_release.close()
-        except Exception as error:
-            raise error
+    template_file_path = parent_dir + RELEASE_GITHUB_TEMPLATE_PATH
+    template_file_name = RELEASE_GITHUB_TEMPLATE_NAME
+    create_template_file(template_file_path, template_path, template_file_name)
 
     # Step 5: CHANGELOG.md
     tag_version = None
     git_repository = parent_dir + ".git"
     if os.path.exists(git_repository):
         try:
-            changelog_process = subprocess.run(["git", "describe", "--abbrev=0", "--match='v*.*.*'", "--tags"], stdout=subprocess.PIPE ,cwd=parent_dir)
-            process_message = changelog_process.stdout
-            if process_message:
-                tag_version = process_message
+            changelog_process = subprocess.run(
+                ["git", "describe", "--tags"], capture_output=True, cwd=parent_dir
+            )
+            tag_str = changelog_process.stdout.decode("utf-8")
+            if tag_str.startswith("v"):
+                tag_str = tag_str[1:]
+            if tag_str:
+                tag_version = tag_str
         except subprocess.CalledProcessError as e:
-             print(e.returncode, e.output)
+            print(e.returncode, e.output)
         except Exception as e:
-            print("Exception during git describe tag execution")
+            print("Exception during git describe tag execution: %e", e)
 
     changelog = parent_dir + "CHANGELOG.md"
     if not os.path.exists(changelog):
-        f = open(changelog, "w", encoding="utf-8")
+        f = open(changelog, "a")
+        f.close()
+        f = open(changelog, "w")
         if not tag_version:
-            f.write('## 0.0.1 (Unreleased)')
+            f.write("## 0.0.1 (Unreleased)")
         else:
-            tag_version = '## ' + tag_version + ' (Unreleased)'
+            tag_version = "## " + tag_version[:-1] + "(Unreleased)"
             f.write(tag_version)
         f.close()
     print("Changelog sucessfully added")
